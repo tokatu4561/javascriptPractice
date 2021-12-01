4d47e933b9f4d5fb0c5557233ae73029186b8918
tokatu4561 <ryobsk4561@gmail.com>
2021-12-01 08:27:54 -0800

    SFC

---
+++ vue_practice/vue-cli/test-cli/src/modules/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      テストです
+      {{testData}}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TestComponent',
+    data(){
+        return {
+            testData: 'テストdataです'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
