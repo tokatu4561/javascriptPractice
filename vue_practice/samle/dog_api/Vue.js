ab21f6d47e8dcadca65c91ebb945c723a878e90a
tokatu4561 <ryobsk4561@gmail.com>
2021-12-01 07:05:33 -0800

    dogApi

--- vue_practice/samle/dog_api/Vue.js
+++ vue_practice/samle/dog_api/Vue.js
@@ -3,7 +3,35 @@ new Vue({
     vuetify: new Vuetify(),
     data(){
       return {
-        dogTypes: ['akita', 'beagle', 'pekinse', 'pag']
+        dogTypes: ['akita', 'beagle', 'pekinese', 'pug'],
+        dogUrl: '',
+        dogImages:'',
+        isShow:false
       }
+    },
+    methods: {
+        async fetchDogImages(index){
+            const that = this
+            this.dogUrl =`https://dog.ceo/api/breed/${this.dogTypes[index]}/images`
+            const response = await fetch(this.dogUrl, {method:'get'})
+            .then( response => {
+                if(response.ok){
+                    that.isShow = true
+                    return response.json()
+                }
+                throw new Error('error')
+
+            }).catch(e => console.log(e.message))
+            
+            this.render(response)
+        },
+        render(response){
+            if(response.message.length > 20){
+              this.dogImages = _(response.message)
+              .shuffle().slice(0, 20).value()
+            } else {
+              this.dogImages = _.shuffle(response.message)
+            }
+          }
     }
   })
