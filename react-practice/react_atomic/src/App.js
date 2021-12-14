c91c844b7de972b7d6c9e76636a0a68f73c8eba8
tokatu4561 <ryobsk4561@gmail.com>
2021-12-14 06:57:48 -0800

    molecule

--- react-practice/react_atomic/src/App.js
+++ react-practice/react_atomic/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
+import { SearchInput } from './components/molecules/SearchInput';
 
 function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <SearchInput></SearchInput>
     </div>
   );
 }
