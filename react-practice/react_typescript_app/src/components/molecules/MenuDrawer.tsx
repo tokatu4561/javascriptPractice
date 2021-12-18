4f2102f04a2ed6462601e5372c6e66b6911c7bce
tokatu4561 <ryobsk4561@gmail.com>
2021-12-17 20:57:20 -0800

    ルーティング機能

--- react-practice/react_typescript_app/src/components/molecules/MenuDrawer.tsx
+++ react-practice/react_typescript_app/src/components/molecules/MenuDrawer.tsx
@@ -1,25 +1,41 @@
 import { memo, VFC } from "react";
 import { Button } from "@chakra-ui/button";
-import { Drawer, DrawerBody, DrawerContent, DrawerOverlay } from "@chakra-ui/modal";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+} from "@chakra-ui/modal";
 
 type Props = {
-    onClose: () => void; 
-    isOpen: boolean; 
-}
+  onClose: () => void;
+  isOpen: boolean;
+  onClickHome: () => void;
+  onClickUserManegement: () => void;
+  onClickSetting: () => void;
+};
 
-export const MenuDrawer:VFC<Props> = memo((props) => {
-    const { onClose, isOpen } = props;
-    return (
-        <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-            <DrawerContent>
-                <DrawerBody p={0} bg="gray.100">
-                    <Button w="100%">TOP</Button>
-                    <Button w="100%">ユーザー一覧</Button>
-                    <Button w="100%">設定</Button>
-                </DrawerBody>
-            </DrawerContent>
-        </DrawerOverlay>
-        </Drawer>
-    )
+export const MenuDrawer: VFC<Props> = memo((props) => {
+  const {
+    onClose,
+    isOpen,
+    onClickHome,
+    onClickSetting,
+    onClickUserManegement,
+  } = props;
+  return (
+    <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerBody p={0} bg="gray.100">
+            <Button onClick={onClickHome} w="100%">
+              TOP
+            </Button>
+            <Button onClick={onClickUserManegement}w="100%">ユーザー一覧</Button>
+            <Button onClick={onClickSetting}w="100%">設定</Button>
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  );
 });
--- react-practice/react_typescript_app/src/components/organisms/layout/Header.tsx
+++ react-practice/react_typescript_app/src/components/organisms/layout/Header.tsx
@@ -1,43 +1,69 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Button } from "@chakra-ui/button";
 import { Box, Flex, Heading, Link } from "@chakra-ui/layout";
-import { memo, VFC } from "react";
+import { memo, useCallback, VFC } from "react";
 
 import { useDisclosure } from "@chakra-ui/hooks";
 
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
-import { MenuDrawer } from "../../molecules/MenuDrawer" 
+import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useHistory } from "react-router";
 
 export const Header: VFC = memo(() => {
-  const { isOpen, onOpen, onClose} = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+
+  const onClickHome = useCallback(() => {
+    history.push("/");
+  }, []);
+  const onClickUserManegement = useCallback(() => {
+    history.push("/home/user_manegement");
+  }, []);
+  const onClickSetting = useCallback(() => {
+    history.push("/home/setting");
+  }, []);
+
   return (
     <>
-    <Flex
-      as="nav"
-      bg="teal.500"
-      color="gray.50"
-      align="center"
-      justify="between"
-      padding={{ base: 3, md: 5 }}
-    >
-      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-          ユーザー管理アプリ
-        </Heading>
-      </Flex>
       <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
         align="center"
-        fontSize="sm"
-        flexGrow={2}
-        display={{ base: "none", md: "flex" }}
+        justify="between"
+        padding={{ base: 3, md: 5 }}
       >
-        <Box pr={4}>
-          <Link>ユーザー一覧</Link>
-        </Box>
-        <Link>設定</Link>
+        <Flex
+          align="center"
+          as="a"
+          mr={8}
+          _hover={{ cursor: "pointer" }}
+          onClick={onClickHome}
+        >
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザー管理アプリ
+          </Heading>
+        </Flex>
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link onClick={onClickUserManegement}>ユーザー一覧</Link>
+          </Box>
+          <Link onClick={onClickSetting}>設定</Link>
+        </Flex>
+        <MenuIconButton onOpen={onOpen}></MenuIconButton>
       </Flex>
-      <MenuIconButton onOpen={onOpen}></MenuIconButton>
-    </Flex>
-    <MenuDrawer onClose={onClose} isOpen={isOpen}></MenuDrawer>
+      <MenuDrawer
+        onClose={onClose}
+        isOpen={isOpen}
+        onClickHome= {onClickHome}
+        onClickUserManegement={onClickUserManegement}
+        onClickSetting={onClickSetting}
+      ></MenuDrawer>
     </>
   );
 });
