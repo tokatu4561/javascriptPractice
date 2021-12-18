b1e1f423cf86819c22642532719bbcca65c6e727
tokatu4561 <ryobsk4561@gmail.com>
2021-12-17 21:16:39 -0800

    ログイン画面作成

--- react-practice/react_typescript_app/src/components/pages/Login.tsx
+++ react-practice/react_typescript_app/src/components/pages/Login.tsx
@@ -1,7 +1,19 @@
+import { Box, Button, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton"
 
 export const Login:VFC = memo(() => {
     return (
-        <p>ログインページ</p>
+        <Flex align="center" justify="center" height="100vh">
+            <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+                <Heading as="h1" size="lg" textAlign="center">ユーザー管理アプリ</Heading>
+                <Divider my={4}/>
+                <Stack spacing={3} py={4} px={10}>
+                <Input placeholder="ユーザーID"></Input>
+                <PrimaryButton>ログイン</PrimaryButton>
+                </Stack>
+            </Box>
+        </Flex>
     )
 });
