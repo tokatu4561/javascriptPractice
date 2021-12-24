4fd815502722032ef86d84ec6d9aa0fb2b5767ed
tokatu4561 <ryobsk4561@gmail.com>
2021-12-24 07:03:06 -0800

    decorata validate

--- typescript_practice/typescript_project_practice/src/app.ts
+++ typescript_practice/typescript_project_practice/src/app.ts
@@ -1,3 +1,35 @@
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number; 
+}
+
+//validattion
+function validate(validatableInput: Validatable){
+    let isValid = true;
+    if(validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if(validatableInput.minLength && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if(validatableInput.maxLength && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if(validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if(validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value >= validatableInput.max;
+    }
+
+    return isValid;
+}
+
+
 // autobind decorator
 function autobind(
   _target: any,
@@ -14,6 +46,7 @@ function autobind(
     }
     return adjDescriptor;
 }
+
 
 // project input class
 class ProjectInput {
@@ -53,15 +86,58 @@ class ProjectInput {
   
   private submitHandler(event: Event) {
     event.preventDefault();
+    this.clearInput();
+  }
+
+  private gatherUserInput(): [string, string, number] | void{
+    const enterdTitle = this.titleInputElement.value;
+    const enterdDescription = this.descriptionInputElement.value;
+    const enterdManday = this.mandayInputElement.value;
+
+    const titleValidatable: Validatable = {
+        value: enterdTitle,
+        required: true,
+    }
+    const descriptionValidatable: Validatable = {
+        value: enterdTitle,
+        required: true,
+        minLength: 5
+    }
+    const mandayValidatable: Validatable = {
+        value: +enterdTitle,
+        required: true,
+        min:1,
+        max: 1000
+    }
+    if (
+        validate(titleValidatable) ||
+        validate(descriptionValidatable) ||
+        validate(mandayValidatable)
+    ) {
+        alert('入力値が正しくありません');
+        return;
+    }else {
+        return [enterdTitle, enterdDescription, +enterdManday];
+    }
   }
 
   @autobind
   private configure() {
     this.element.addEventListener("submit", this.submitHandler);
+    const userInput = this.gatherUserInput();
+    if(Array.isArray(userInput)){
+        const [title, desc, manday] = userInput;
+    }
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+
+  private clearInput() {
+      this.titleInputElement.value = '';
+      this.descriptionInputElement.value = '';
+      this.mandayInputElement.value = '';
   }
 }
 
