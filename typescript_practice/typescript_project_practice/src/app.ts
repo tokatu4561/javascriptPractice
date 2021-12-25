6cab6df7a14b7f59692ea7f67837a6583a8b1c9b
tokatu4561 <ryobsk4561@gmail.com>
2021-12-24 21:40:08 -0800

    １つのプロジェクト表示用のクラス

--- typescript_practice/typescript_project_practice/src/app.ts
+++ typescript_practice/typescript_project_practice/src/app.ts
@@ -9,20 +9,28 @@ class Project {
     public id: string,
     public title: string,
     public description: string,
-    public monday: number,
+    public manday: number,
     public status: ProjectStatus
   ) {}
 }
 
-type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
+
+class State<T>  {
+    protected listeners: Listener<T>[] = [];
+    addListener(listenerFn: Listener<T>) {
+        this.listeners.push(listenerFn);
+      }
+}
 
 //プロジェクト状態管理
-class ProjectState {
-  private listeners: Listener[] = [];
+class ProjectState extends State <Project> {
   private project: Project[] = [];
   private static instance: ProjectState;
 
-  private constructor() {}
+  private constructor() {
+      super();
+  }
 
   static getInstance() {
     if (this.instance) {
@@ -30,10 +38,6 @@ class ProjectState {
     }
     this.instance = new ProjectState();
     return this.instance;
-  }
-
-  addListener(listenerFn: Listener) {
-    this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, manday: number) {
@@ -154,6 +158,35 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   }
 }
 
+//プロジェクトアイテム
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+    private project: Project;
+
+    get manday() {
+        if(this.project.manday < 20) {
+            return this.project.manday.toString() + '人日';
+        } else {
+            return (this.project.manday / 20).toString +'人月';
+        }
+    }
+
+    constructor (hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {}
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.manday.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProject: Project[] = [];
@@ -194,9 +227,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     )! as HTMLUListElement;
     listEl.innerHTML = "";
     for (const prjItem of this.assignedProject) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(listEl.id, prjItem);
     }
   }
 }
