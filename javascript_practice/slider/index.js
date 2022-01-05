c598def442c15132d731d8a019c15c5f725c72fb
tokatu4561 <ryobsk4561@gmail.com>
2022-01-04 16:23:58 -0800

    slider レイアウト

---
+++ javascript_practice/slider/index.js
@@ -0,0 +1,36 @@
+const target = document.getElementById("target");
+const sliderItems = document.querySelectorAll(
+  "#target .slider-data .slider-item"
+);
+
+let sliderShow = document.createElement("div");
+let main = document.createElement("div");
+let extra = document.createElement("div");
+
+sliderShow.classList.add("col-12", "d-flex", "flex-nowrap", "overflow-hiddens");
+main.classList.add("main", "full-width");
+extra.classList.add("extra", "full-width");
+
+main.append(sliderItems[0]);
+
+sliderShow.append(main);
+sliderShow.append(extra);
+target.append(sliderShow);
+
+let controls = document.createElement("div");
+controls.classList.add("offset-5", "mt-2");
+
+let leftBtn = document.createElement("button");
+leftBtn.classList.add("btn", "btn-light");
+leftBtn.innerHTML = "<";
+
+let rightBtn = document.createElement("button");
+rightBtn.classList.add("btn", "btn-light");
+rightBtn.innerHTML = ">";
+
+controls.append(leftBtn);
+controls.append(rightBtn);
+target.append(controls);
+
+//setAttribute(属性名, 属性値)を使ってindexの初期値を設定します。
+main.setAttribute("data-index", "0");
