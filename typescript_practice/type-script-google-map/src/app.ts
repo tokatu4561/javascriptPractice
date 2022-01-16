9bc802d6c844c16026709f9c22d9269b55e1ca66
tokatu4561 <ryobsk4561@gmail.com>
2022-01-15 20:02:37 -0800

    add searchInput submit event

--- typescript_practice/type-script-google-map/src/app.ts
+++ typescript_practice/type-script-google-map/src/app.ts
@@ -1,1 +1,9 @@
-// ここにコードを書きます
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddressHandler(e: Event) {
+  e.preventDefault();
+  const enterdAdress = addressInput.value;
+}
+
+form?.addEventListener("submit", searchAddressHandler);
