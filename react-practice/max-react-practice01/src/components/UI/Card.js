3a7b8f46ab98ebf2e5d805d820797b848be757b1
tokatu4561 <ryobsk4561@gmail.com>
2022-01-26 06:57:29 -0800

    add Card Ui comopnent

---
+++ react-practice/max-react-practice01/src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Card.module.css";
+
+export const Card = (props) => {
+  return (
+    <div className={`${props.className} ${classes.card}`}>{props.children}</div>
+  );
+};
