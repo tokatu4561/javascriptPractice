e7ad13f561fb46fab57be9b142bb9265da60afb3
tokatu4561 <ryobsk4561@gmail.com>
2022-03-03 07:17:13 -0800

    add redux slice

--- react-practice/redux-practice2/src/components/Cart/CartButton.js
+++ react-practice/redux-practice2/src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
 import classes from './CartButton.module.css';
 
+import {uiActions} from "../../store/ui-slice";
+
 const CartButton = (props) => {
+
+  const toggleCartHandler = () => {
+
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
--- react-practice/redux-practice2/src/index.js
+++ react-practice/redux-practice2/src/index.js
@@ -1,6 +1,13 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import { store } from "./store/index";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
