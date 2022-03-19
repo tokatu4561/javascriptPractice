aa0c510de3b0ace80cd746a210b2a423ed316dc3
tokatu4561 <ryobsk4561@gmail.com>
2022-03-19 05:50:42 -0700

    mobail menu

---
+++ javascript_practice/animation/slide-animation/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      el.classList.add("inview");
+    }
+  };
+
+  const so = new ScrollObserver(".cover-slide", cb);
+});
