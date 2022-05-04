424dbaa48c01650f70d8bdc388bdfeb3865a6284
tokatu4561 <ryobsk4561@gmail.com>
2022-05-03 20:01:40 -0700

    add routing

--- react-practice/react-meetup/src/App.tsx
+++ react-practice/react-meetup/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ react-practice/react-meetup/src/pages/AllMeetups.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllMeetupsPage = () => {
+  return <div></div>;
+};
+
+export default AllMeetupsPage;
